0b396d151e3a678a430152e393845a3c6ad1464c
Nathan Marques de Aguiar Brandão <108840944+NathanMarques2001@users.noreply.github.com>
2023-02-09 20:42:55 -0300

    Update Main.java

--- AEDIII/Main.java
+++ AEDIII/Main.java
@@ -2,8 +2,32 @@ package Revision;
 
 public class Main {
   public static void main(String[] args) {
-    Employee nathan = new Employee("nathan", 800);
+    /*
+Liste todos os funcionários em ordem alfabética
+Realizar uma remoção de um registro existente
+Tentar uma remoção de um registro não existente*/
+    Employee one = new Employee("Iago", 1100);
+    Employee two = new Employee("Edson", 2200);
+    Employee three = new Employee("Rosa", 5000);
+    Employee four = new Employee("Larissa", 1500);
+    Employee five = new Employee("Lais", 800);
 
-    System.out.print(nathan.getName());
+    List EmployersList = new List();
+
+    EmployersList.add(one);
+    EmployersList.add(two);
+    EmployersList.add(three);
+    EmployersList.add(four);
+    EmployersList.add(five);
+
+    EmployersList.printList();
+
+    EmployersList.containInList("rosa");
+    EmployersList.containInList("nathan");
+
+    EmployersList.search(0);
+    EmployersList.search(5);
+
+    EmployersList.orderByName();
   }
 }
