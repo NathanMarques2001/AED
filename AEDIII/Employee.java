992d73dc04cd49d4817c6f9169a44d33a5ffbb78
Nathan Marques de Aguiar Brandão <108840944+NathanMarques2001@users.noreply.github.com>
2023-02-09 20:42:15 -0300

    Update Employee.java

--- AEDIII/Employee.java
+++ AEDIII/Employee.java
@@ -1,28 +1,28 @@
 package Revision;
 
-class Employee {
-    private String name;
-    private double salary;
+class Employee{
+  private String name;
+  private double salary;
 
-    public Employee(String name, double salary) {
-        this.name = name;
-        this.salary = salary;
-    }
+  public Employee(String name, double salary) {
+    this.name = name;
+    this.salary = salary;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public double getSalary() {
-        return salary;
-    }
+  public double getSalary() {
+    return salary;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
+  public void setSalary(double salary) {
+    this.salary = salary;
+  }
 
 }
