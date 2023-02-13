99b54d326e5b720d7811e1637c51b2ccb716d2ee
Nathan Marques de Aguiar Brandão <108840944+NathanMarques2001@users.noreply.github.com>
2023-02-12 23:28:32 -0300

    Update List.java

--- AEDIII/List.java
+++ AEDIII/List.java
@@ -44,25 +44,29 @@ public class List {
 	}
 
 	public void removeEmployeeForNumber(int position) {
+		System.out.println("\nRemovendo o funcionario na posicao " + position
+				+ "...\n---------------------------------------------");
 		if (Employers[position] != null) {
-			System.out.println("\nRemovendo o funcionario na posicao " + position
-					+ "...\n---------------------------------------------");
 			System.out.println("Funcionario " + Employers[position].getName()
 					+ " removido da lista.\n=============================================");
 			Employers[position] = null;
 			length--;
-			for (int i = 0; i < Employers.length; i++) {
-				if (i < length) {
-					if (Employers[i] == null && Employers[i + 1] != null) {
-						Employers[i] = Employers[i + 1];
-						Employers[i + 1] = null;
-					}
-				}
-			}
+			relocationInTheList();
 		} else {
-			System.out.println("\nA posicao " + position
+			System.out.println("A posicao " + position
 					+ " esta vazia, entao nao posso\nremover ninguem. Favor verificar o numero\nda pessoa que deseja remover!\n=============================================");
 		}
+	}
+
+	// Organiza a lista apos remover alguem
+	private Employee[] relocationInTheList() {
+		for (int i = 0; i < length; i++) {
+			if (Employers[i] == null && Employers[i + 1] != null) {
+				Employers[i] = Employers[i + 1];
+				Employers[i + 1] = null;
+			}
+		}
+		return Employers;
 	}
 
 	public void printSearchEmployeeForName(String name) {
@@ -75,13 +79,9 @@ public class List {
 	}
 
 	private boolean searchEmployeeForName(String name) {
-		for (int i = 0; i < Employers.length; i++) {
-			if (Employers[i] != null) {
-				if (Employers[i].getName().equalsIgnoreCase(name)) {
-					return true;
-				}
-			} else {
-				break;
+		for (int i = 0; i < length; i++) {
+			if (Employers[i].getName().equalsIgnoreCase(name)) {
+				return true;
 			}
 		}
 		return false;
@@ -94,36 +94,24 @@ public class List {
 	public void printList() {
 		System.out.println(
 				"\nLista de funcionarios e respectivos salarios\n---------------------------------------------");
-		for (int i = 0; i < Employers.length; i++) {
-			if (Employers[i] != null) {
-				System.out.println(i + " -> " + Employers[i].getName() + " - $" + Employers[i].getSalary());
-			} else {
-				break;
-			}
+		int i = 0;
+		while (i < length) {
+			System.out.println(i + " -> " + Employers[i].getName() + " - R$" + Employers[i].getSalary());
+			i++;
 		}
 		System.out.println("=============================================");
 	}
 
 	public Employee[] alphabeticalOrder() {
 		Employee auxEmployee;
-		for (int i = 0; i < Employers.length; i++) {
-			if (Employers[i] != null) {
-				for (int j = 0; j < Employers.length; j++) {
-					if (Employers[j] != null) {
-						if (Employers[i].getName().compareTo(Employers[j].getName()) < 0) {
-							auxEmployee = Employers[i];
-							Employers[i] = Employers[j];
-							Employers[j] = auxEmployee;
-						}
-					} else {
-						break;
-					}
-
+		for (int i = 0; i < length; i++) {
+			for (int j = 0; j < length; j++) {
+				if (Employers[i].getName().compareTo(Employers[j].getName()) < 0) {
+					auxEmployee = Employers[i];
+					Employers[i] = Employers[j];
+					Employers[j] = auxEmployee;
 				}
-			} else {
-				break;
 			}
-
 		}
 		printAlphabeticalOrder(Employers);
 		return Employers;
@@ -132,12 +120,8 @@ public class List {
 	public void printAlphabeticalOrder(Employee[] arr) {
 		System.out
 				.println("\nLista de funcionarios em ordem alfabetica\n---------------------------------------------");
-		for (int i = 0; i < Employers.length; i++) {
-			if (Employers[i] != null) {
-				System.out.println(i + " -> " + Employers[i].getName() + " - $" + Employers[i].getSalary());
-			} else {
-				break;
-			}
+		for (int i = 0; i < length; i++) {
+			System.out.println(i + " -> " + Employers[i].getName() + " - R$" + Employers[i].getSalary());
 		}
 		System.out.println("=============================================");
 	}
