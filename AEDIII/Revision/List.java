810627804575be5ad38cfea7133e01cb52d2d8b4
Nathan Marques de Aguiar Brandão <108840944+NathanMarques2001@users.noreply.github.com>
2023-02-15 01:21:40 -0300

    Create List.java

---
+++ AEDIII/Revision/List.java
@@ -0,0 +1,30 @@
+package Revision;
+
+public class List {
+	private Employee Employers[] = new Employee[100];
+    private int length = 0;
+
+    public boolean isEmpty() {
+    	return true;
+    }
+    
+    public void enqueue(Employee newEmployee) {
+    	
+    }
+    
+    public void dequeue(String name) {
+    	
+    }
+    
+    public boolean contain(String name) {
+    	return true;
+    }
+    
+    public void print() {
+    	
+    }
+    
+    public int size() {
+    	return 0;
+    }
+}
