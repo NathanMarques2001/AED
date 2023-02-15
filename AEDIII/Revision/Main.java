35bb45e1eb8363502221e69aed1d185fe6955391
Nathan Marques de Aguiar Brandão <108840944+NathanMarques2001@users.noreply.github.com>
2023-02-15 01:21:13 -0300

    Update Main.java

--- AEDIII/Revision/Main.java
+++ AEDIII/Revision/Main.java
@@ -1,41 +1,45 @@
-
 package Revision;
 
 public class Main {
 	public static void main(String[] args) {
-
-		Employee one = new Employee("Iago", 1100);
-		Employee two = new Employee("Edson", 2200);
-		Employee three = new Employee("Rosa", 5000);
-		Employee four = new Employee("Larissa", 1500);
-		Employee five = new Employee("Lais", 800);
-		Employee six = new Employee("Bruno", 5200);
-		Employee seven = new Employee("Bruna", 7000);
-
-		List EmployersList = new List();
-
-		EmployersList.addNewEmployee(one);
-		EmployersList.addNewEmployee(two);
-		EmployersList.addNewEmployee(three);
-		EmployersList.addNewEmployee(four);
-		EmployersList.addNewEmployee(five);
-		EmployersList.addNewEmployee(six);
-		EmployersList.addNewEmployee(seven);
-
-		EmployersList.printList();
-
-		EmployersList.printSearchEmployeeForName("rosa");
-		EmployersList.printSearchEmployeeForName("nathan");
-
-		EmployersList.searchEmployeeForNumber(0);
-		EmployersList.searchEmployeeForNumber(7);
-
-		EmployersList.alphabeticalOrder();
-
-		EmployersList.removeEmployeeForNumber(2);
-		EmployersList.removeEmployeeForNumber(10);
-
-		EmployersList.printList();
+		
+		  Company CompanyData = new Company();
+		  
+		  CompanyData.addNewEmployee(new Employee("Iago", 1100));
+		  CompanyData.addNewEmployee(new Employee("Edson", 2200));
+		  CompanyData.addNewEmployee(new Employee("Rosa", 5000));
+		  CompanyData.addNewEmployee(new Employee("Larissa", 1500));
+		  CompanyData.addNewEmployee(new Employee("Lais", 800));
+		  CompanyData.addNewEmployee(new Employee("Bruno", 5200));
+		  CompanyData.addNewEmployee(new Employee("Bruna", 7000));
+		  
+		  CompanyData.printCompanyData();
+		  
+		  CompanyData.printSearchEmployeeForName("rosa");
+		  CompanyData.printSearchEmployeeForName("nathan");
+		  
+		  CompanyData.searchEmployeeForNumber(0);
+		  CompanyData.searchEmployeeForNumber(7);
+		  
+		  CompanyData.alphabeticalOrder();
+		  
+		  CompanyData.removeEmployeeForNumber(2);
+		  CompanyData.removeEmployeeForNumber(10);
+		  
+		  CompanyData.printCompanyData();
+		 
+		////////////////////////////////////
+		
+		Stack EmployeeStack = new Stack();
 			
+		EmployeeStack.push(new Employee("Iago", 1100));
+		EmployeeStack.push(new Employee("Edson", 2200));
+		EmployeeStack.push(new Employee("Rosa", 5000));
+		EmployeeStack.push(new Employee("Larissa", 1500));
+		EmployeeStack.push(new Employee("Lais", 800));
+		EmployeeStack.push(new Employee("Bruno", 5200));
+		EmployeeStack.push(new Employee("Bruna", 7000));
+		
+		EmployeeStack.print();
 	}
 }
