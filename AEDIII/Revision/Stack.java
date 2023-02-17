b8561a3e767dc7484263ebcd28669a5e91649234
Nathan Marques de Aguiar Brandão <108840944+NathanMarques2001@users.noreply.github.com>
2023-02-16 21:30:52 -0300

    Update Stack.java

--- AEDIII/Revision/Stack.java
+++ AEDIII/Revision/Stack.java
@@ -1,69 +1,82 @@
 package Revision;
 
 public class Stack {
-  private Employee Employers[] = new Employee[100];
-  private int length = 0;
+    private Employee Employers[] = new Employee[100];
+    private int length = 0;
 
-  public boolean isEmpty() {
-    return true;
-  }
-
-  public void push(Employee newEmployee) {
-    //Como esta sendo inserido -> Iago, Edson, Rosa, Larissa, Lais, Bruno, Bruna
-    //Como deve ser imprimido -> Bruna, Bruno, Lais, Larissa, Rosa, Edson, Iago
-    if(length < Employers.length){
-      if(Employers[0] == null){
-        Employers[0] = newEmployee;
-        length++;
-      } else {
-        for (int i = 0; i < length; i++) {
-          Employers[length - i] = Employers[length - i - 1];
+    private boolean isEmpty(){
+        if(Employers[0] == null){
+            return true;
         }
-        Employers[0] = newEmployee;
-        length++;
-      }
+        return false;
     }
-  }
 
-  public void pop() {
-    if (Employers[0] != null) {
-      for (int i = 0; i < length; i++) {
-        Employers[i] = Employers[i + 1];
-      }
-      Employers[length] = null;
-      length--;
+    public void printIsEmpty(){
+        System.out.println("\nVerificando se a pilha esta vazia...\n---------------------------------------------");
+        if(isEmpty()){
+            System.out.println("A pilha esta vazia!\n=============================================");
+        } else {
+            System.out.println("A nao pilha esta vazia!\n=============================================");
+        }
     }
-  }
 
-  public void printContain(String name){
-    System.out.println(
-            "\nBuscando "+name+" na pilha...\n---------------------------------------------");
-    if(contain(name)){
-      System.out.println("Encontrei " + name + " na pilha!\n=============================================");
-    } else{
-      System.out.println("Nao encontrei " + name + " na pilha!\n=============================================");
+
+    public void push(Employee newEmployee) {
+        //Como esta sendo inserido -> Iago, Edson, Rosa, Larissa, Lais, Bruno, Bruna
+        //Como deve ser imprimido -> Bruna, Bruno, Lais, Larissa, Rosa, Edson, Iago
+        if(length < Employers.length){
+            if(Employers[0] == null){
+                Employers[0] = newEmployee;
+                length++;
+            } else {
+                for (int i = 0; i < length; i++) {
+                    Employers[length - i] = Employers[length - i - 1];
+                }
+                Employers[0] = newEmployee;
+                length++;
+            }
+        }
     }
-  }
 
-  private boolean contain(String name) {
-    for (int i = 0; i < length; i++) {
-      if (name.equalsIgnoreCase(Employers[i].getName())) {
-        return true;
-      }
+    public void pop() {
+        if (Employers[0] != null) {
+            for (int i = 0; i < length; i++) {
+                Employers[i] = Employers[i + 1];
+            }
+            Employers[length] = null;
+            length--;
+        }
     }
-    return false;
-  }
 
-  public void print() {
-    System.out.println(
-            "\nLista de funcionarios e respectivos salarios\n---------------------------------------------");
-    for (int i = 0; i < length; i++) {
-      System.out.println((length - i) + " -> " + Employers[i].getName() + " - R$" + Employers[i].getSalary());
+    public void printContain(String name){
+        System.out.println(
+                "\nBuscando "+name+" na pilha...\n---------------------------------------------");
+        if(contain(name)){
+            System.out.println("Encontrei " + name + " na pilha!\n=============================================");
+        } else{
+            System.out.println("Nao encontrei " + name + " na pilha!\n=============================================");
+        }
     }
-    System.out.println("=============================================");
-  }
 
-  public int size() {
-    return length;
-  }
+    private boolean contain(String name) {
+        for (int i = 0; i < length; i++) {
+            if (name.equalsIgnoreCase(Employers[i].getName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void print() {
+        System.out.println(
+                "\nLista de funcionarios e respectivos salarios\n---------------------------------------------");
+        for (int i = 0; i < length; i++) {
+            System.out.println((length - i) + " -> " + Employers[i].getName() + " - R$" + Employers[i].getSalary());
+        }
+        System.out.println("=============================================");
+    }
+
+    public int size() {
+        return length;
+    }
 }
