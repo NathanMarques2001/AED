70026ed788b281720c994a121bd0958e581f2589
Nathan Marques de Aguiar Brandão <108840944+NathanMarques2001@users.noreply.github.com>
2023-02-16 21:30:31 -0300

    Update List.java

--- AEDIII/Revision/List.java
+++ AEDIII/Revision/List.java
@@ -1,65 +1,76 @@
 package Revision;
 
 public class List {
-  private Employee Employers[] = new Employee[100];
-  private int length = 0;
+    private Employee Employers[] = new Employee[100];
+    private int length = 0;
 
-  public boolean isEmpty() {
-    return true;
-  }
-
-  public void enqueue(Employee newEmployee) {
-      if(length <= Employers.length){
-        Employers[length] = newEmployee;
-        length++;
-        System.out.println("\nFuncionario " + newEmployee.getName()
-                + " adicionado a lista.\n=============================================");
-      } else {
-        System.out.print("A lista ja alcancou sua capacidade maxima!");
-      }
-  }
-
-  public void dequeue() {
-    System.out.println("\nRemovendo o ultimo funcionario da lista...\n---------------------------------------------");
-      if(Employers[length - 1] != null){
-        System.out.println("Funcionario " + Employers[length - 1].getName()
-                + " removido da lista.\n=============================================");
-        Employers[length - 1] = null;
-        length--;
-      } else {
-        System.out.println("A lista esta vazia, entao nao e possivel remover ninguem!\n=============================================");
-      }
-  }
-
-  public void printContain(String name){
-    System.out.println(
-            "\nBuscando "+name+" na lista...\n---------------------------------------------");
-    if(contain(name)){
-      System.out.println("Encontrei " + name + " na lista!\n=============================================");
-    } else{
-      System.out.println("Nao encontrei " + name + " na lista!\n=============================================");
+    private boolean isEmpty() {
+        if(Employers[0] == null){
+            return true;
+        }
+        return false;
     }
-  }
 
-  private boolean contain(String name) {
-    for (int i = 0; i < length; i++) {
-      if(name.equalsIgnoreCase(Employers[i].getName())){
-        return true;
-      }
+    public void printIsEmpty(){
+        System.out.println("\nVerificando se a lista esta vazia...\n---------------------------------------------");
+        if(isEmpty()){
+            System.out.println("A lista esta vazia!\n=============================================");
+        } else {
+            System.out.println("A lista nao esta vazia!\n=============================================");
+        }
     }
-    return false;
-  }
-
-  public void print() {
-    System.out.println(
-            "\nFuncionarios e seus respectivos salarios\n---------------------------------------------");
-    for (int i = 0; i < length; i++) {
-      System.out.println((i+1) + " -> " + Employers[i].getName() + " - R$" + Employers[i].getSalary());
+    public void enqueue(Employee newEmployee) {
+        if(length <= Employers.length){
+            Employers[length] = newEmployee;
+            length++;
+            System.out.println("\nFuncionario " + newEmployee.getName()
+                    + " adicionado a lista.\n=============================================");
+        } else {
+            System.out.print("A lista ja alcancou sua capacidade maxima!");
+        }
     }
-    System.out.println("=============================================");
-  }
 
-  public int size() {
-    return length;
-  }
+    public void dequeue() {
+        System.out.println("\nRemovendo o ultimo funcionario da lista...\n---------------------------------------------");
+        if(Employers[length - 1] != null){
+            System.out.println("Funcionario " + Employers[length - 1].getName()
+                    + " removido da lista.\n=============================================");
+            Employers[length - 1] = null;
+            length--;
+        } else {
+            System.out.println("A lista esta vazia, entao nao e possivel remover ninguem!\n=============================================");
+        }
+    }
+
+    public void printContain(String name){
+        System.out.println(
+                "\nBuscando "+name+" na lista...\n---------------------------------------------");
+        if(contain(name)){
+            System.out.println("Encontrei " + name + " na lista!\n=============================================");
+        } else{
+            System.out.println("Nao encontrei " + name + " na lista!\n=============================================");
+        }
+    }
+
+    private boolean contain(String name) {
+        for (int i = 0; i < length; i++) {
+            if(name.equalsIgnoreCase(Employers[i].getName())){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void print() {
+        System.out.println(
+                "\nFuncionarios e seus respectivos salarios\n---------------------------------------------");
+        for (int i = 0; i < length; i++) {
+            System.out.println((i+1) + " -> " + Employers[i].getName() + " - R$" + Employers[i].getSalary());
+        }
+        System.out.println("=============================================");
+    }
+
+    public int size() {
+        return length;
+    }
 }
