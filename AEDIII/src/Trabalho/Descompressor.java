4fecd8d9d2adb5b8e8ef3bf84f9f133a4d626066
Nathan Marques de Aguiar Brandão <108840944+NathanMarques2001@users.noreply.github.com>
2023-06-07 01:19:21 -0300

    Update Descompressor.java

--- AEDIII/src/Trabalho/Descompressor.java
+++ AEDIII/src/Trabalho/Descompressor.java
@@ -4,51 +4,59 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Descompressor {
-    
-    public static void decompress(String inputFilePath, String outputFilePath) {
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(inputFilePath));
-            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath));
-            
-            // Lê o cabeçalho PGM
-            String magicNumber = reader.readLine();
-            String comment = reader.readLine();
-            String dimensions = reader.readLine();
-            String maxValue = reader.readLine();
-            writer.write(magicNumber + "\n");
-            writer.write(comment + "\n");
-            writer.write(dimensions + "\n");
-            writer.write(maxValue + "\n");
-            
-            // Lê os valores comprimidos e faz a descompressão com RLE
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] tokens = line.split(" ");
-                for (int i = 0; i < tokens.length; i += 2) {
-                    int count = Integer.parseInt(tokens[i]);
-                    int pixel = Integer.parseInt(tokens[i + 1]);
-                    
-                    for (int j = 0; j < count; j++) {
-                        writer.write(pixel + " ");
-                    }
-                }
-            }
-            
-            reader.close();
-            writer.close();
-            
-            System.out.println("Descompressão concluída.");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    
-    public static void main(String[] args) {
-    	Scanner sc = new Scanner(System.in);
-    	System.out.print("Digite o nome da imagem -> ");
-    	String imageName = sc.nextLine();
-        String inputFilePath = "src/Trabalho/imagens/"+imageName+"_comprimido.pgm";
-        String outputFilePath = "src/Trabalho/imagens/"+imageName+"_descomprimida.pgm";
-        decompress(inputFilePath, outputFilePath);
-    }
+
+	public static void main(String[] args) throws IOException {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Insira o nome da imagem -> ");
+		String nomeArquivo = sc.nextLine();
+		sc.close();
+
+		String pathArquivoPrincipal = "src/Trabalho/imagens/" + nomeArquivo + "-comp-RLE.pgm";
+		String pathArquivoDescomprimido = "src/Trabalho/imagens/" + nomeArquivo + "-desc-RLE.pgm";
+
+		DescomprimePGM(pathArquivoPrincipal, pathArquivoDescomprimido);
+
+	}
+
+	private static void DescomprimePGM(String pathArquivoPrincipal, String pathArquivoDescomprimido)
+			throws IOException {
+		BufferedReader in = new BufferedReader(new FileReader(pathArquivoPrincipal));
+		BufferedWriter out = new BufferedWriter(new FileWriter(pathArquivoDescomprimido));
+
+		String linha = in.readLine();
+
+		int inicioCabecalho = linha.indexOf("'");
+		int finalCabecalho = linha.lastIndexOf("'");
+		String cabecalho = linha.substring(inicioCabecalho, finalCabecalho + 1);
+		cabecalho = cabecalho.replaceAll("'", "");
+		String valores = linha.replace(cabecalho, "");
+
+		String dadosCabecalho[] = cabecalho.split(",");
+
+		String numeroMagico = dadosCabecalho[0];
+		String comentario = dadosCabecalho[1];
+		String dimensao = dadosCabecalho[2] + " " + dadosCabecalho[3];
+		String valorMaximo = dadosCabecalho[4];
+
+		out.write(numeroMagico);
+		out.newLine();
+		out.write(comentario);
+		out.newLine();
+		out.write(dimensao);
+		out.newLine();
+		out.write(valorMaximo);
+		out.newLine();
+		
+		int atual = 0;
+		int quantidade = 0;
+		
+		while(valores.indexOf("@") != -1) {
+			
+		}
+		
+		in.close();
+		out.close();
+	}
 }
